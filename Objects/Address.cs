93bb45c02259e89c089f1ec42913bcf7c1584b70
Minh Phuong <mphuong@kent.edu>
2017-02-10 09:08:57 -0800

    Create new public type Address in namespace AddressBook.Objects in Address.cs

--- Objects/Address.cs
+++ Objects/Address.cs
@@ -1,9 +1,16 @@
 namespace AddressBook.Objects
 {
-  public class Detail
+  public class Address
   {
     private string _street;
     private string _city;
     private string _state;
+
+    public Address (string street, string city, string state)
+    {
+      _street = street;
+      _city = city;
+      _state = state;
+    }
   }
 }
