d05731927688c795642b078b7368fb2dd3655c20
Minh Phuong <mphuong@kent.edu>
2017-02-10 08:49:11 -0800

    Create new public class Contact in namespace AddressBook.Objects in Contact.cs

---
+++ Objects/Contact.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace AddressBook.Objects
+{
+  public class Contact
+  {
+    private string _name;
+    private int _id;
+    private Detail _detail;
+  }
+}
